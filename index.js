ec31f93e8667a2bcf3dd86b461912751aa902461
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-03-05 14:17:44 +0530

    Create server to get json data

--- index.js
+++ index.js
@@ -13,6 +13,30 @@ const getHtml = () => {
 </html>`;
 };
 
+const getJson = () => {
+  return {
+    slideshow: {
+      author: "Yours Truly",
+      date: "date of publication",
+      slides: [
+        {
+          title: "Wake up to WonderWidgets!",
+          type: "all",
+        },
+        {
+          items: [
+            "Why <em>WonderWidgets</em> are great",
+            "Who <em>buys</em> WonderWidgets",
+          ],
+          title: "Overview",
+          type: "all",
+        },
+      ],
+      title: "Sample Slide Show",
+    },
+  };
+};
+
 const sendResponse = (res, status, contentType, content) => {
   res.writeHead(status, { "Content-Type": contentType });
   res.end(content);
@@ -20,11 +44,12 @@ const sendResponse = (res, status, contentType, content) => {
 
 const routes = {
   "/html": (res) => sendResponse(res, 200, "text/html", getHtml()),
+  "/json": (res) =>
+    sendResponse(res, 200, "application/json", JSON.stringify(getJson())),
 };
 
 const server = http.createServer((req, res) => {
   const path = req.url;
-  console.log(path);
 
   if (req.method != "GET") {
     sendResponse(res, 405, "text/plain", "Method Not Allowed");
